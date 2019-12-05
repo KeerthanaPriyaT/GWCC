0434b699bc072b9992e70bc89339eaa0c53618ca
KeerthanaPriyaT <55872082+KeerthanaPriyaT@users.noreply.github.com>
2019-12-05 13:04:08 +0530

    Add files via upload

---
+++ vars/sonarqube.groovy
@@ -0,0 +1,11 @@
+if call(Map params){
+ def dirc = params.Dir
+ def hsturl = params.Urll
+ def pName = params.ProjName
+ def login = params.Login
+ def source = params.Source
+
+dir(dirc){
+sh "./gwb sonarqube -Dsonar.host.url=${hsturl} -Dsonar.projectName=${pName} -Dsonar.login=${login} -Dsonar.sources=${source}" 
+}
+}
